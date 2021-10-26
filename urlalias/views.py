7be575d6d44e06d716b9c6f92145033fec58dc55
javim24 <javi.mosconi@gmail.com>
2021-10-25 21:00:25 -0300

    Implementacion de funcion que recibe el url y genera el alias

--- urlalias/views.py
+++ urlalias/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
 from .forms import URLForm2
 from .models import URLAlias
 import nanoid
@@ -7,7 +7,7 @@ import nanoid
 # Create your views here.
 _NANO_DICT = "abcdefz-"
 
-def index(request):
+def index_vieja(request):
 
 
     if request.method == "POST":
@@ -30,6 +30,27 @@ def index(request):
 	
 
     return render(request, "urlalias/home.html", ctx)
+
+"""
+Crear función que reciba un POST desde el front, con el link que se desea convertir. 
+
+La función genera un alias y lo guarda en la base de datos. 
+
+Luego, responde un json con el alias generado"""
+
+def index(request):
+    if request.method == 'POST':
+         # Se puede acceder a la informacion del POST de forma
+        form = URLForm2(request.POST)
+        if form.is_valid():
+            fullurl = form.cleaned_data.get("fullurl")
+            url = URLAlias(fullurl=fullurl)
+            alias = nanoid.generate(size=5)
+            url.alias=alias
+            url.save()
+
+            #ahora falta responder el json
+            return JsonResponse({'alias': alias, 'fullurl': fullurl})
 
 def resolve_alias(request, alias):
     resultado = URLAlias.objects.get(alias=alias)
